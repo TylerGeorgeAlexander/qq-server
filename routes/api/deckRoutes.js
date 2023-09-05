7f3ca136d1701500288fd7cc9230146ee4b4b462
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-09-05 15:16:20 -0400

    Fix: relative paths

--- routes/api/deckRoutes.js
+++ routes/api/deckRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const deckController = require("../controllers/deckController");
+const deckController = require("../../controllers/deckController");
 const { extractUserId, requireAuth } = require('../../middleware/authMiddleware');
 
 // Route to fetch all decks
