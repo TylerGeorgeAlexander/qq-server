36747e739eaaa1918577e5e6d61a85c815d3b6e7
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-09-05 13:54:07 -0400

    feat: Mount API routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // Import API routes
 const userRoutes = require('./api/userRoutes');
+const deckRoutes = require('./api/deckRoutes');
 
 // Mount API routes
 router.use('/users', userRoutes);
+router.use('/decks', deckRoutes);
 
 module.exports = router;
